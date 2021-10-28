97025595102f1a1f29ad82e8761624c44e2a2313
unknown <danihunt8@.com>
2021-10-28 04:19:28 +0300

    First Commit

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -76,21 +76,21 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME' : 'stockmgmt',
-        'USER' : 'myusername',
-        'PASSWORD' : 'Danihuny7'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' : 'django.db.backends.mysql',
+#         'NAME' : 'stockmgmt',
+#         'USER' : 'myusername',
+#         'PASSWORD' : 'Danihuny7'
+#     }
+# }
 
 
 # Password validation
